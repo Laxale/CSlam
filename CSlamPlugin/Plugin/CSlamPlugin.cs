d18d1699d99c902b292fa9bc4ff943ebdc1abec0
Laxale <axel.1777@mail.ru>
2016-10-31 16:12:48 +0300

    Added CSlamPlugin

---
+++ CSlamPlugin/Plugin/CSlamPlugin.cs
@@ -0,0 +1,31 @@
+﻿// Created by Laxale 31.10.2016
+//
+//
+
+
+using System;
+
+
+namespace CSlamPlugin.Plugin 
+{
+    using Freengy.Base;
+    using Freengy.GamePlugin;
+    using Freengy.GamePlugin.Interfaces;
+
+
+    /// <summary>
+    /// <see cref="IGamePlugin"/> implementer
+    /// </summary>
+    public class CSlamPlugin : IGamePlugin 
+    {
+        public Type ExportedViewType { get; }
+
+        public string Name { get; }
+
+        public string DisplayedName { get; }
+
+        public Guid Id { get; }
+
+        public string GameIconSource { get; }
+    }
+}
